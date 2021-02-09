4f93d00bc5deede89e0308d6043a34bd2de383fe
Konstantin Dmitrievskiy <>
2021-02-09 23:50:53 +0600

    Add note model

---
+++ NoteApp/Model/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  NoteApp
+//
+//  Created by Konstantin Dmitrievskiy on 09.02.2021.
+//  Copyright © 2021 Konstantin Dmitrievskiy. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
