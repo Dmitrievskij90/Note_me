5db73a1db6437d8347c316accf51f46a58e22fb3
Konstantin Dmitrievskiy <>
2021-02-09 22:47:26 +0600

    Initial commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Konstantin Dmitrievskiy on 09.02.2021.
+//  Copyright © 2021 Konstantin Dmitrievskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
