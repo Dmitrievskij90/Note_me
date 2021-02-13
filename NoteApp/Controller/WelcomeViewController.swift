0749e4bc9a868477637ea68afcb5947164231c3d
Konstantin Dmitrievskiy <>
2021-02-14 01:51:51 +0600

    Add welcomeVC

---
+++ NoteApp/Controller/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  NoteApp
+//
+//  Created by Konstantin Dmitrievskiy on 14.02.2021.
+//  Copyright © 2021 Konstantin Dmitrievskiy. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
